9eb335044576f97e27e4d520947261510420c722
SPR1NG090 <gijs.van.vugt8@gmail.com>
2022-06-15 11:51:29 +0200

    comunicatie

--- src/App.js
+++ src/App.js
@@ -5,19 +5,33 @@ import Searchbar from "./Searchbar";
 class App extends React.Component {
     state = { locatie: ""};
 
+    something = false;
+
     makeApiCall = searchTerm => {
-        const BASE_URL = "http://127.0.0.1:8000/api/locatie/";
-        axios.get(BASE_URL + searchTerm).then(res=> {
-            console.log(res);
+        const BASE_URL = "http://127.0.0.1:8000/api/";
+        axios.get(BASE_URL + 'locaties').then(res=> {
+            console.log(res.data);
+            // get id lala en maak dit res
+
+            
+            document.getElementById('Title--locatie').innerHTML = res.data[0]['name']
+            
+            
+            
         });
     };
+
+    componentDidMount(){
+        this.makeApiCall()
+    }
     
     render() {
         return (
         <main>
 
-            <Searchbar onSubmit={this.makeApiCall}/>
-        
+            {/* <Searchbar onSubmit={this.makeApiCall}/> */}
+            <p id="Title--locatie"></p>
+            
         </main>
         );
     }
