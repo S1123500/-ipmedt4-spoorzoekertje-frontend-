2ef099f608c45f063efbce9060fab44e1100989b
S1123500 <s1123500@student.hsleiden.nl>
2022-06-22 13:29:38 +0200

    Revert "toevoegen frontpage"
    This reverts commit 73a3e0e9faa2532ad3fba0eb9b0d37b360c6508f.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,15 @@
 import React from 'react';
 import Overlay from './components/Overlay';
 import './App.css';
-import Header from './components/Header'
-import Section from './components/Section'
 import ApiGET from './hooks/ApiGET';
 import ApiPOST from './hooks/ApiPOST';
 
 
 function App() {
   return (
-    /* player page
-      <section className="App">
-          <Overlay playertype="player" questsleft="69" currentquest="Heel veel zuipen" currentlocation="Lazaru's"> </Overlay>
-        </section>
-        */
-
-    /* runner page
-      <section className="App">
-          <Overlay playertype="runner" questsleft="69" currentquest="Heel veel zuipen" currentlocation="Lazaru's"> </Overlay>
-        </section>
-        */
-
-        <main>
-            <Header  />
-            <Section />
-          </main>
+    <section className="App">
+      <Overlay playertype="player" questsleft="69" currentquest="Heel veel zuipen" currentlocation="Lazaru's"> </Overlay>
+    </section>
   );
 }
 
