73a3e0e9faa2532ad3fba0eb9b0d37b360c6508f
sam <verlaansam@gmail.com>
2022-06-22 12:36:36 +0200

    toevoegen frontpage

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import Overlay from './components/Overlay';
 import './App.css';
+import Header from './components/Header'
+import Section from './components/Section'
 import ApiGET from './hooks/ApiGET';
 import ApiPOST from './hooks/ApiPOST';
 
 
 function App() {
   return (
-    <section className="App">
-      <Overlay playertype="player" questsleft="69" currentquest="Heel veel zuipen" currentlocation="Lazaru's"> </Overlay>
-    </section>
+    /* player page
+      <section className="App">
+          <Overlay playertype="player" questsleft="69" currentquest="Heel veel zuipen" currentlocation="Lazaru's"> </Overlay>
+        </section>
+        */
+
+    /* runner page
+      <section className="App">
+          <Overlay playertype="runner" questsleft="69" currentquest="Heel veel zuipen" currentlocation="Lazaru's"> </Overlay>
+        </section>
+        */
+
+        <main>
+            <Header  />
+            <Section />
+          </main>
   );
 }
 
