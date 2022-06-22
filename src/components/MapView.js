933a1c2c06b0e2e250542d126eccdcacf2c77da8
yoepvond <vandiepenyoep@gmail.com>
2022-06-22 12:42:32 +0200

    Comments

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -8,23 +8,23 @@ class MapView extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentLocation: { lat: 52.160114, lng: 4.497010 }, //center of Leiden
-      zoom: 15,
+      currentLocation: { lat: 52.160114, lng: 4.497010 }, //center of Leiden, regelt waar de kaart moet staan.
+      zoom: 15, // zorgt dat de kaart zo ingezoomed is als nodig is.
     }
   }
 
-  render() {
-    const { currentLocation, zoom } = this.state;
+  render() { // zorgt dat de kaart in de pagina geladen wordt.
+    const { currentLocation, zoom } = this.state; 
 
-    return (
-      <Map center={currentLocation} zoom={zoom}>
+    return ( //pull via leaflet de locatie en zet de de kaart via Mapview current location en zoom komen van Mapview.
+      <Map center={currentLocation} zoom={zoom}> 
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
         />
 
-        <Markers venues={data.venues}/>
-      </Map>
+        <Markers venues={data.venues}/> 
+      </Map> // De markers komen van de data.json uit de assets folder. 
     );
   }
 }
