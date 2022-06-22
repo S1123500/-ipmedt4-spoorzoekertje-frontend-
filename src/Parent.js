f4b88a75254bc3c8e922e9ef1ab5a2673082a63a
yoepvond <vandiepenyoep@gmail.com>
2022-06-22 17:04:02 +0200

    Fix voor roomcode log

--- src/Parent.js
+++ src/Parent.js
@@ -12,47 +12,35 @@ class Parent extends Component {
         }
     }
 
-    toggleInGame = () => {
+    toggleInGame = (formData) => {
         this.setState({ in_game: !this.state.in_game });
     };
 
-
-
-    
     formCallback = (formData) =>{
-        console.log("form data: " + formData)
         this.setState({room_code: formData});
-        console.log("state data: " + this.state.room_code);
+        console.log("form data: " + formData);
     }
 
 
     render() {
         const { in_game } = this.state, { setInGame } = this;
         if (!in_game) {
+            console.log("state data: " + this.state.room_code);
             return (
                 <Login in_game={false} onClick={this.toggleInGame} formCallback={this.formCallback}/>
+                
             );
         }
 
         else {
             if (!this.is_player) {
                 return (
-<<<<<<< HEAD
-                    <Overlay playertype="runner" questsleft="4" currentquest="Vind de volgende Kroeg" currentlocation="Centraal" />
-                );
-            }
-            if (this.is_player) {
-                return ( 
-                    <Overlay  />
-                    
-=======
                     <Overlay playertype="runner" questsleft="69" currentquest="Heel veel zuipen" currentlocation="Lazaru's"/>
                 );
             }
             if (this.is_player) {
                 return (
                     <Overlay playertype="runner" questsleft="69" currentquest="Heel veel zuipen" currentlocation="Lazaru's"/>
->>>>>>> f982823a6f9ca1a556679317e641518aea4b96f9
                 );
             }
         }
