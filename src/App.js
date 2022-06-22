86d25299ca96100d774dc49c8d8a015bab0e0936
S1123500 <s1123500@student.hsleiden.nl>
2022-06-22 13:34:09 +0200

    Revert "Fixed broken dev repo"
    This reverts commit ea75fa82073e8bf666cae062a2c84f0b25b26934.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React from 'react';
-// import Overlay from './components/GameOverlay';
-import Login from './components/Login';
-import './css/App.css';
-// import ApiGET from './hooks/ApiGET';
-// import ApiPOST from './hooks/ApiPOST';
-
+import Overlay from './components/Overlay';
+import './App.css';
+import ApiGET from './hooks/ApiGET';
+import ApiPOST from './hooks/ApiPOST';
 
 
 function App() {
   return (
     <section className="App">
-      {/* <Overlay playertype="player" questsleft="69" currentquest="Heel veel zuipen" currentlocation="Lazaru's"> </Overlay> */}
-      <Login />
+      <Overlay playertype="player" questsleft="69" currentquest="Heel veel zuipen" currentlocation="Lazaru's"> </Overlay>
     </section>
   );
 }
--- src/components/ContentTop.js
+++ src/components/ContentTop.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import "../css/Overlay.css";
+import "./Overlay.css";
 
 const ContentTop = (props) => {
 
